f8cd396ffc2ed9011ae21e3860d16031596a0d52
Matthew Sullivan <143848513+Daceit@users.noreply.github.com>
2025-04-02 14:25:10 -0700

    Add files via upload

---
+++ Image.h
@@ -0,0 +1,71 @@
+#ifndef IMAGE_H 
+
+#define IMAGE_H 1
+
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#include "Bridges.h"
+#include "ColorGrid.h"
+
+using namespace bridges;
+using namespace std;
+
+class Image {
+	private:
+        // image dimensions
+		int width, height, maxVal;
+
+        // pointer to the dynamically allocated image array
+		int *image_array;
+
+		// you will need an array to hold the processed results
+		int *processed_array;
+
+	public:
+		// constructor - creates an empty image object,
+		Image() {
+		}
+        // creates an image object by reading the input image
+		Image(string input_file){
+		};
+        //creates an image object using the given dimensions
+		Image(int width, int height) {
+		}
+
+		// destructor
+		~Image() { 
+		}
+        // TO DO: you might need setters/getters for accessing data items,
+		// image pixel, etc
+
+		// reads an image from  the given input image file
+		void read(string file_name) {
+		}
+
+		// writes image to the Bridges colorgrid for visualization
+		void setColorGrid (ColorGrid& cg) {
+		}
+
+		// convert to grayscale
+		void toGrayscale () {
+		}
+		// flip image horizontally
+		void flipHorizontal() {
+		}
+
+		// remove the blue component
+		void flattenBlue() {
+		}
+
+		// use the complement of red (wraparound)
+		void negateRed() {
+		}
+
+		// reads a new image and blends with existing image
+		void blend (string file_name, float blend_factor) {
+		}
+};
+
+#endif
---
+++ ImageProcess.cpp
@@ -0,0 +1,17 @@
+#include "Image.h"
+
+using namespace bridges;
+
+int main() {
+
+	Bridges bridges(140, "BRIDGES_USER_ID", "BRIDGES_API_KEY");
+
+	bridges.setTitle("Image Processing");
+	bridges.setDescription("Students are provided with sample images in a simple text format that they read and display using the ColorGrid datatype.");
+
+	//TODO: the main program where you call various image processing
+	// operations.
+
+	return 0;
+}
+
