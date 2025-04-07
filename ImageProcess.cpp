2f35f728b65e76ddfe68cd679ee78df9a098a1d0
Madla <1001348@my.scccd.edu>
2025-04-07 13:48:52 -0700

    IP.cpp

--- ImageProcess.cpp
+++ ImageProcess.cpp
@@ -4,8 +4,7 @@ using namespace bridges;
 
 int main() {
 
-	Bridges bridges(1,"Madzilla23", "16463411788"3);
-
+	Bridges bridges(1,"Madzilla23", "164634117883");
 	bridges.setTitle("Image Processing");
 	bridges.setDescription("Students are provided with sample images in a simple text format that they read and display using the ColorGrid datatype.");
 	//TODO: the main program where you call various image processing
