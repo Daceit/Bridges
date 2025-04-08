132b71965b0028a0c10cd2ded2e22cbf08fba355
1014109 <matthewsullivan05@gmail.com>
2025-04-08 16:15:19 -0700

    ImageProcess

--- ImageProcess.cpp
+++ ImageProcess.cpp
@@ -1,7 +1,6 @@
 #include "Image.h"
 
 using namespace bridges;
-
 int main() {
 
 	Bridges bridges(1,"Madzilla23", "164634117883");
@@ -9,7 +8,7 @@ int main() {
 	bridges.setDescription("Students are provided with sample images in a simple text format that they read and display using the ColorGrid datatype.");
 	//TODO: the main program where you call various image processing
 	// operations.
-	Image img(IMG_8464.ppm);
+	Image img("IMG_8464.ppm");
 
 	img.toGrayscale();
 
